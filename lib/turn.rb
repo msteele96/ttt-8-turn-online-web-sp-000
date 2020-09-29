d3660d53f12ecff73c37d12fe7c766dd09749bea
Marc Steele <marcsteele2014@gmail.com>
2020-09-29 19:29:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,5 @@
 def valid_move?(board, index)
-<<<<<<< HEAD
   if index < 0 || index > 8
-=======
-  if index <= 0 || index >= 8
->>>>>>> 832a22197014cc7629b546a32d522247bd244a5e
     return false
   elsif position_taken?(board, index) == true
     "That space is already taken."
