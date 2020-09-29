d913d73bf8ec3b7b332915c02c5b74d283f2a01c
Marc Steele <marcsteele2014@gmail.com>
2020-09-29 19:25:45 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def valid_move?(board, index)
+<<<<<<< HEAD
+  if index < 0 || index > 8
+=======
+  if index <= 0 || index >= 8
+>>>>>>> 832a22197014cc7629b546a32d522247bd244a5e
+    return false
+  elsif position_taken?(board, index) == true
+    "That space is already taken."
+    return false
+  elsif position_taken?(board, index) == false
+    return true
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+return (input.to_i - 1)
+end
+
+def move(board, index, character = "X")
+
+  def update_array_at_with(array, index, value)
+  array[index] = value
+  end
+
+  update_array_at_with(board, index , character)
+
+  return board
+end
