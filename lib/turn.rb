2f0a909d0b4511889edfed1f81768786de96cde7
Marc Steele <marcsteele2014@gmail.com>
2020-09-29 19:37:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,5 @@
 def valid_move?(board, index)
-  if index < 0 || index > 8
-    return false
-  elsif position_taken?(board, index) == true
-    "That space is already taken."
-    return false
-  elsif position_taken?(board, index) == false
-    return true
-  end
+  
 end
 
 def display_board(board)
@@ -15,10 +8,6 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-return (input.to_i - 1)
 end
 
 def move(board, index, character = "X")
@@ -34,4 +23,27 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  input = gets.strip
+  index = input.to_i - 1
+  if index < 0 || index > 8
+    #ask for new input
+  elsif position_taken?(board, index) == true
+    "That space is already taken."
+    #ask for new input
+  elsif position_taken?(board, index) == false
+    return true
+    #make move
+    def move(board, index, character = "X")
+
+      def update_array_at_with(array, index, value)
+      array[index] = value
+      end
+
+      update_array_at_with(board, index , character)
+
+      return board
+    end
+    move(board, index)
+  end
+  
 end
